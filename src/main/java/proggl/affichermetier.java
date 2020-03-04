14aae42ea9d95a42c65b7710650b791b9e36f10a
Edouard Longueville <edouard.longueville@ens.uvsq.fr>
2020-03-04 11:02:23 +0100

    1st commit

--- src/main/java/proggl/affichermetier.java
+++ src/main/java/proggl/affichermetier.java
@@ -1,4 +1,20 @@
 package proggl;
 
+import java.time.LocalDateTime;
+
 public class affichermetier {
+
+    public affichermetier(){}
+
+    public void affichedebut(){
+
+        System.out.println(LocalDateTime.now() + "DebutdeUnemethodemetier");
+
+    }
+
+    public void affichefin(){
+
+        System.out.println(LocalDateTime.now() + "FindeUnemethodemetier");
+
+    }
 }
--- src/main/java/proggl/uneclassemetier.java
+++ src/main/java/proggl/uneclassemetier.java
@@ -1,4 +1,18 @@
 package proggl;
 
 public class uneclassemetier {
+
+    public affichermetier affiche;
+
+    public uneclassemetier(){
+
+        this.affiche = new affichermetier();
+
+    }
+
+    public void unemethodemetier(){
+        affiche.affichedebut();
+        affiche.affichefin();
+    }
+
 }
